aa70f356e7332d5648925616a18d97bb260aaa88
PhilAnges <48360145+PhilAnges@users.noreply.github.com>
2020-02-27 15:53:00 -0600

    Good File.

---
+++ FirstPersonCamera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    public float smooth = 5;
+    public float lookSpeed = 1;
+    private float lookLeftRight = 0;
+    private float lookUpDown = 0;
+    public GameObject target;
+
+    public float gravity = -1;
+    public float speed = 1;
+    
+
+    
+    void Start()
+    {
+        
+    }
+
+    // This script should be put onto the player object, with the player object being the GameObject target. The camera should be attached to the player.
+    void Update()
+    {
+        
+        lookLeftRight = (Input.GetAxis("Mouse X") * lookSpeed) + lookLeftRight;
+        lookUpDown = (Input.GetAxis("Mouse Y") * lookSpeed) + lookUpDown;
+        //float x = Mathf.Clamp(lookLeftRight, -170f, 170f);
+        float y = Mathf.Clamp(lookUpDown, -90f, 90f);
+
+        Quaternion rotate = Quaternion.Euler(-y, lookLeftRight, 0);
+        target.transform.localRotation = Quaternion.Slerp(target.transform.localRotation, rotate, smooth);
+    }
+    //This lets the player object move. However, this does not move based on the local roatation, meaning that A and D will always move the player in the same global direction. Not currently sure how to fix this.
+    private void FixedUpdate()
+    {
+        float moveHoriz = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+
+        transform.translate(moveHoriz, 0, moveVert);
+    }
+
+
+}
